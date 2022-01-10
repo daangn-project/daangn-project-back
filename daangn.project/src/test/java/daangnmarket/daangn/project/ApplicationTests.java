25afc11f073e2d893c0d191c197a84ca4559a6aa
sunghyeon <josh5734@naver.com>
2022-01-10 16:13:21 +0900

    initial commit

---
+++ daangn.project/src/test/java/daangnmarket/daangn/project/ApplicationTests.java
@@ -0,0 +1,13 @@
+package daangnmarket.daangn.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
