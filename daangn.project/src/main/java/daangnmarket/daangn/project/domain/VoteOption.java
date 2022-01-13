791e69cf15155d640ef68a0442c85c0c7469ba29
shinkyuchul <kc960923@naver.com>
2022-01-13 13:56:02 +0900

    add vote-entity

--- daangn.project/src/main/java/daangnmarket/daangn/project/domain/VoteOption.java
+++ daangn.project/src/main/java/daangnmarket/daangn/project/domain/VoteOption.java
@@ -1,4 +1,24 @@
 package daangnmarket.daangn.project.domain;
 
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
 public class VoteOption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "VOTE_OPTION_ID")
+    private Long id;
+
+    private String voteOptionName;
+
+    private Integer voteTotalCount;
+
+    @OneToMany
+    private List<Member> participateMember = new ArrayList<>();
+
 }
