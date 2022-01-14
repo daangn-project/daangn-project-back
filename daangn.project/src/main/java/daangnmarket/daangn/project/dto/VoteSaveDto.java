a575a9cfd05ab697c53d53f17851004f176d1552
sunghyeon <josh5734@naver.com>
2022-01-14 15:52:03 +0900

    voteDto implements

---
+++ daangn.project/src/main/java/daangnmarket/daangn/project/dto/VoteSaveDto.java
@@ -0,0 +1,7 @@
+package daangnmarket.daangn.project.dto;
+
+import lombok.Data;
+
+@Data
+public class VoteSaveDto {
+}
---
+++ daangn.project/src/main/java/daangnmarket/daangn/project/dto/VoteUpdateDto.java
@@ -0,0 +1,8 @@
+package daangnmarket.daangn.project.dto;
+
+import lombok.Data;
+
+@Data
+public class VoteUpdateDto {
+
+}
